d6a9a5de43ec0747a0f24aa75ea417fd2a3394b8
PIKSSy <nekedtitok123@gmail.com>
2023-03-07 00:17:39 +0100

    Languages

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -10,4 +10,26 @@ class LanguageController extends Controller
     public function index(){
         return Language::all();
     }
+
+    public function destroy(Request $request)
+    {
+        $langIds = $request->input('languages');
+        if (!empty($langIds)) {
+            Language::whereIn('lang_id', $langIds)->delete();
+            return response()->json(['message' => 'Languages deleted successfully.']);
+        } else {
+            return response()->json(['message' => 'No languages selected for deletion.']);
+        }
+    }
+
+    public function store(Request $request)
+    {
+        $languages = $request->input('languages');
+        foreach ($languages as $language) {
+            Language::create(['language' => $language]);
+        }
+    
+        return response()->json(['message' => 'Languages created successfully']);
+    }
+
 }
